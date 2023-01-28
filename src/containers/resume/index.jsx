968e78f22608fabfbe3f606b53c8bf0948d07ef7
Mahmoud Awad.M <Dev-Mahmoud123>
2023-01-28 12:48:41 +0200

    create resume page

--- src/containers/resume/index.jsx
+++ src/containers/resume/index.jsx
@@ -1,13 +1,41 @@
 import React from "react";
 import PageHeaderContent from "../../components/pageHeaderContent";
+import { resumeData } from "./utils";
+import "./style.scss"
 
 const Resume = () => {
   return (
     <section id="resume" className="resume">
       <PageHeaderContent
         headerTitle="My Resume"
-        icon={<i className="fa-solid fa-circle-info"></i>}
+        icon={<i className="fa-brands fa-black-tie"></i>}
       />
+      <div className="timeline">
+        <div className="timeline_education">
+          <h3 className="timeline_education_header-text">Education</h3>     
+            {resumeData.education.map((item) => {
+              return (
+                <div className="timeline_education_content">
+                  <h4>{item.title}</h4>
+                  <p>{item.date}</p>
+                  <p>{item.description}</p>
+                </div>
+              );
+            })}
+        </div>
+        <div className="timeline_experience">
+          <h3 className="timeline_experience_header-text">Experience</h3>
+          {resumeData.experience.map((item) => {
+            return (
+              <div className="timeline_experience_content">
+                <h4>{item.title}</h4>
+                <p>{item.date}</p>
+                <p>{item.description}</p>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </section>
   );
 };
---
+++ src/containers/resume/utils.js
@@ -0,0 +1,29 @@
+export const resumeData = {
+  experience: [
+    {
+      title: "data entry",
+      date: "Mars , 2022 - present",
+      description: `I worked as data entry insert and update and delete data in system`,
+    },
+    {
+      title: "mobile developer",
+      date: "Feb , 2021 - Jun , 2021",
+      description: `I worked as flutter developer , developed ui
+                   and make app responsive in all screens and get data from api`,
+    },
+  ],
+  education :[
+      {
+            title: 'chemistry science',
+            date: "2014 - 2018",
+            description: `Holds a Bachelor’s degree from the Faculty of
+             Science from Zagazig University in 2018 `
+      },
+      {
+            title: "high school certificate",
+            date: "2011 - 2014",
+            description:`Holds the Egyptian General Secondary 
+            Certificate from El Mahmoudia Secondary School`
+      }
+  ]
+};
