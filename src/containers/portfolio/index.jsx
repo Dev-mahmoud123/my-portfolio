e2b05199662b92e79acd1b06336032cc691142a7
Mahmoud Awad.M <Dev-Mahmoud123>
2023-02-03 16:59:13 +0200

    create portfolio page

--- src/containers/portfolio/index.jsx
+++ src/containers/portfolio/index.jsx
@@ -1,13 +1,72 @@
 import React from "react";
+import { useState } from "react";
 import PageHeaderContent from "../../components/pageHeaderContent";
+import "./style.scss";
+import { filterData, portfolioData } from "./utils";
 
 const Portfolio = () => {
+  const [filter, setFilter] = useState("all");
+  const [hoverValue, setHover] = useState(null);
+  const handleFilter = (categoryName) => {
+    setFilter(categoryName);
+  };
+  const handleHover = (index) => {
+    setHover(index);
+  };
+  console.log("================");
+  console.log(filter);
+  console.log("==================");
+
+  const filterProjects =
+    filter === "all"
+      ? portfolioData
+      : portfolioData.filter((item) => item.category === filter);
+
   return (
     <section id="portfolio" className="portfolio">
       <PageHeaderContent
         headerTitle="My Portfolio"
         icon={<i className="fa-solid fa-circle-info"></i>}
       />
+      <div className="portfolio_content">
+        <ul className="portfolio_content_select-category">
+          {filterData.map((item) => {
+            return (
+              <li
+                className={item.cat === filter ? "active" : ""}
+                key={item["filter-id"]}
+                onClick={() => handleFilter(item.cat)}
+              >
+                {item.cat}
+              </li>
+            );
+          })}
+        </ul>
+        <div className="portfolio_content_cards">
+          {filterProjects.map((item, index) => {
+            return (
+              <div
+                className="portfolio_content_cards_item"
+                key={item.id}
+                onMouseMove={() => handleHover(index)}
+                onMouseLeave={() => handleHover(null)}
+              >
+                <div className="portfolio_content_cards_item_image">
+                  <img src={item.image} alt={item.name} />
+                </div>
+                <div className="overlay">
+                  {index === hoverValue && (
+                    <div>
+                      <p>{item.name}</p>
+                      <a href={item.link} target="_blank" rel="noreferrer">Visit</a>
+                    </div>
+                  )}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
     </section>
   );
 };
---
+++ src/containers/portfolio/utils.js
@@ -0,0 +1,99 @@
+import  imageOne  from "../../images/e-commerce.png";
+import imageTwo  from "../../images/movie.png";
+import  imageThree from "../../images/dashboard.png";
+import  imageFour  from "../../images/product-management-sys.png";
+import  imageFive from "../../images/template-two.png";
+import  imageSix from "../../images/template-three.png";
+import  imageSeven  from "../../images/template-four.png";
+import  imageEight  from "../../images/template-five.png";
+import  imageNine from "../../images/movie-app.png";
+import  imageTen from "../../images/ecommerce-app.png";
+
+
+export const portfolioData = [
+  {
+    id: 1,
+    name: "new collections",
+    image: imageOne,
+    category: "web",
+    link: 'https://new-collections.netlify.app/'
+  },
+  {
+    id: 2,
+    name: "movie website",
+    image: imageTwo,
+    category: "web",
+    link: 'https://mah-react-movie.netlify.app/'
+  },
+  {
+    id: 3,
+    name: "dashboard",
+    image: imageThree,
+    category: "web",
+    link: 'https://dev-mahmoud123.github.io/dashboard-design/'
+  },
+  {
+    id: 4,
+    name: "management system",
+    image: imageFour,
+    category: "web",
+    link: 'https://dev-mahmoud123.github.io/project-with-crud-operation/'
+  },
+  {
+    id: 5,
+    name: "template two",
+    image: imageFive,
+    category: "web",
+    link: 'https://dev-mahmoud123.github.io/HTML_AND_CSS_TEMPLETE2/'
+  },
+  {
+    id: 6,
+    name: "template three",
+    image: imageSix,
+    category: "web",
+    link: 'https://dev-mahmoud123.github.io/HTML_AND_CSS_template3/'
+  },
+  {
+    id: 7,
+    name: "template four",
+    image: imageSeven,
+    category: "web",
+    link: 'https://dev-mahmoud123.github.io/bondi-website-with-bootstrap/'
+  },
+  {
+    id: 8,
+    name: "template five",
+    image: imageEight,
+    category: "web",
+    link: 'https://dev-mahmoud123.github.io/design-with-html-and-css-and-js/'
+  },
+  {
+    id: 9,
+    name: "Movie App",
+    image: imageNine,
+    category: "app",
+    link: 'https://github.com/Dev-mahmoud123/movie-app'
+  },
+  {
+    id: 10,
+    name: "shop app",
+    image: imageTen,
+    category: "app",
+    link: 'https://github.com/Dev-mahmoud123/Shop-App'
+  },
+];
+
+export const filterData = [
+  {
+    "filter-id": 1,
+    cat: "all",
+  },
+  {
+    "filter-id": 2,
+    cat: "web",
+  },
+  {
+    "filter-id": 3,
+    cat: "app",
+  },
+];
